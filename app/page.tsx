daa7d0746eecd8602d3aaf79378273bfd83b796d
Damian Padilla <199727366+DavieDevs@users.noreply.github.com>
2025-11-19 12:30:20 -0600

    feat(Banner): created spacer banner with image (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import ExploreCategories from "./components/ExploreCategories";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <ExploreCategories />
+      <Banner />
     </div>
   );
 }
